d2e4af5ca270f6f42e85b76f9099bcfe61b9a6ee
Igor Harchenko <1305hariok@ukr.net>
2021-10-06 23:09:13 +0300

    Окончено выполнение дз-фидбек

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import "./App.css";
-import Feedback from "./components/feedback/Feedback";
+import React from "react";
+import { Section } from "./components/Section/Section";
+import { FeedbackOptions } from "./components/FeedbackOptions/FeedbackOptions";
+import { Notification } from "./components/Notification/Notification";
+import { Statistics } from "./components/Statistics/Statistics";
 
-function App() {
-  return (
-    <div className="App">
-      <Feedback goodValue={0} neutralValue={0} badValue={0} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = (option) => {
+    this.setState((prevState) => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.bad + this.state.neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return Math.round((good * 100) / total);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
+
+        {good || neutral || bad > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
